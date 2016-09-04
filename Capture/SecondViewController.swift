bf4c084c21480d90b393c1027905e4ecc9613d9f
noobiehacker <davidkwokhochan@gmail.com>
2016-09-03 22:54:40 -0700

    Half way throught implementing camera

--- Capture/SecondViewController.swift
+++ Capture/SecondViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    var captureSession : AVCaptureSession?
+    var stillImageOuput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    @IBOutlet var CameraView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        previewLayer?.frame = CameraView.frame
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +29,55 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        previewLayer?.frame = CameraView.bounds
+    }
+    override func viewWillAppear(animated: Bool) {
+   
+        super.viewWillAppear(animated)
+        do{
+         try self.setUpCamera()
+        }
+        catch{
+        }
+        
+    }
+    
+    func setUpCamera() throws -> Bool{
+    
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error : NSError?
+        var input : AVCaptureDeviceInput? = try AVCaptureDeviceInput(device: backCamera)
+        let bool : Bool? = captureSession!.canAddInput(input!)
+        
+        if error == nil && bool!{
+            
+            captureSession?.addInput(input)
+            
+            stillImageOuput = AVCaptureStillImageOutput()
+            stillImageOuput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            captureSession?.addOutput(stillImageOuput)
+                
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+            previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            
+            CameraView.layer.addSublayer(previewLayer!)
+            captureSession?.startRunning()
+                
+            
+            return true;
+            
+        }
+        return false;
 
+    }
     /*
     // MARK: - Navigation
 
